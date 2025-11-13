d9ed6e104a7dc93297fb26db19b686fe472d9d1d
Bernardo <bernardomoreira.dev@gmail.com>
2025-11-13 00:35:43 -0300

    Primeiro Commit

---
+++ src/Landing.jsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <div className="min-h-screen bg-red-500 text-white flex items-center justify-center text-4xl font-bold">
+      <h1>Tailwind funcionando 🔥</h1>
+    </div>
+  )
+}
