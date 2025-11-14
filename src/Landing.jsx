e35b0a3632bb54c78659542d2bf7670184357fbe
Bernardo <bernardomoreira.dev@gmail.com>
2025-11-14 02:03:38 -0300

    Finish landing

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,10 +3,11 @@ import { faMap, faPeopleGroup, faArrowUpRightDots, faCheck } from '@fortawesome/
 import { faSearchengin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import team from '/team.jpg'
+import Logo from "/Logo-texto.svg"
 
 const Landing = () => {
   return (
-    <main className="bg-[#000b24] text-white">
+    <main className="bg-[#000102] text-white">
         <div className="absolute inset-0 z-0">
             <img src="/Bg-centro.png" className="absolute inset-0 w-full h-screen opacity-70" />
             <div className="absolute inset-0 bg-linear-to-b from-[#0000009b] to-[#000000]"></div>
@@ -14,8 +15,8 @@ const Landing = () => {
 
         <MenuLanding></MenuLanding>
 
-        <section className="z-10 relative text-center py-[10%] flex flex-col justify-center items-center">
-            <h1 className="text-5xl w-[60%] tracking-tighter">Conecte talentos. Requalifique pessoas. <strong>Transforme o trabalho.</strong></h1>
+        <section className="z-10 relative text-center py-[12%] flex flex-col justify-center items-center">
+            <h1 className="text-5xl mt-22 w-[60%] tracking-tighter">Conecte talentos. Requalifique pessoas. <strong>Transforme o trabalho.</strong></h1>
             <p className="w-[40%] mt-6 text-gray-300">O SkillLink é uma plataforma inteligente que forma equipes de alta performance com base em habilidades reais.</p>
             <button className="mt-4 px-8 py-4 rounded-xl backdrop-blur-lg cursor-pointer bg-[#38BDF8] text-black font-semibold">Mude sua equipe conosco</button>
         </section>
@@ -71,7 +72,7 @@ const Landing = () => {
             </div>
         </section>
 
-        <section className="py-16 bg-[#000102] flex flex-col justify-center items-center">
+        <section className="py-16 bg-[#000102] flex flex-col justify-center items-center pb-32">
             <div class="border border-[#182534] w-[70%] flex flex-col justify-center items-center p-16 relative rounded-xl overflow-hidden bg-linear-to-r from-[#090712] via-[#041E23] to-[#090712]">               
                     <h2 class="text-white text-3xl font-bold mb-2">Pronto para construir sua equipe de alta performance?</h2>
                     <p class="text-gray-300 text-base text-center mx-42">
@@ -82,6 +83,17 @@ const Landing = () => {
                     </button>
             </div>
         </section>
+
+        <footer className="bg-[#000102] flex flex-row justify-between px-[15%] border-t border-gray-800 py-12 items-center">
+            <div className="flex flex-row w-[50%] justify-start items-center">
+                <img className="w-[20%]" src={Logo}></img>
+            </div>
+            <div className="text-sm flex flex-row w-[50%] list-none justify-end gap-4">
+                <li>Como Funciona</li>
+                <li>Para Empresas e Times</li>
+                <li>Junte-se a Nós</li>
+            </div>
+        </footer>
 
     </main>
   )
--- src/components/MenuLanding.jsx
+++ src/components/MenuLanding.jsx
@@ -1,18 +1,18 @@
-import Logo from "/Logo-Skill.png"
+import Logo from "/Logo-texto.svg"
 
 const MenuLanding = () => {
     return (
-        <header className="w-full flex flex-row justify-around bg-[#cacaca07] items-center backdrop-blur-2xl text-white text-sm px-32 py-6">
+        <header className="w-full fixed flex flex-row justify-around bg-[#cacaca07] z-100 items-center backdrop-blur-lg text-white text-sm px-[10%] py-10">
             <div className="w-[30%] flex flex-row justify-start items-center">
-                <img className="w-[15%]" src={Logo}></img>
-                </div>
+                <img className="w-[34%]" src={Logo}></img>
+            </div>
             <div className="w-[38%] flex flex-row justify-between list-none">
                 <li>Como Funciona</li>
                 <li>Para Empresas e Times</li>
                 <li>Junte-se a Nós</li>
             </div>
             <div className="w-[30%] flex flex-row justify-end items-center gap-4 ">
-                <button className="px-4 py-2 rounded-lg text-gray-800 bg-white">Entrar</button>
+                <button className="px-6 py-2 rounded-lg bg-[#ffffff15] text-white border border-gray-600">Entrar</button>
                 <button>Cadastrar-se</button>
             </div>
         </header>
